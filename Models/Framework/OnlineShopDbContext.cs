4bb7308add9100d36d516ea0b1a5afa2413eb2c1
pqhuy1987 <pqhuy1987@gmail.com>
2018-02-26 07:50:45 +0700

    Update SQL Entity Framework Install

---
+++ Models/Framework/OnlineShopDbContext.cs
@@ -0,0 +1,23 @@
+namespace Models.Framework
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class OnlineShopDbContext : DbContext
+    {
+        public OnlineShopDbContext()
+            : base("name=OnlineShopDbContext")
+        {
+
+        }
+
+        public virtual DbSet<dbo_Account> dbo_Account { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
