bf51d6de194a64be3917ca5f7750d668d2c8c986
pqhuy1987 <pqhuy1987@gmail.com>
2018-02-27 14:08:09 +0700

    update Login Page

--- ShopOnline/Areas/Admin/Controllers/LoginController.cs
+++ ShopOnline/Areas/Admin/Controllers/LoginController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ShopOnline.Areas.Admin.Models;
+using Models;
+using ShopOnline.Areas.Admin.Code;
 
 namespace ShopOnline.Areas.Admin.Controllers
 {
@@ -10,10 +13,28 @@ namespace ShopOnline.Areas.Admin.Controllers
     {
         //
         // GET: /Admin/Login/
-
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Index(LoginModel model)
+        {
+            var result = new AccountModel().Login(model.UserName, model.Password);
+            if (result && ModelState.IsValid)
+            {
+                SessionHelper.SetSession(new UserSession() { UserName = model.UserName });
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("","Tên đăng nhập không đúng hoặc mật khẩu không đúng.");
+            }
+            return View(model);
         }
 
     }
